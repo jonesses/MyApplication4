2fc3256cb41a6b092cd344e433a48be68762045f
Robert Schweizer <git@rsmd.me>
2016-07-14 10:52:23 +0200

    Add github log message

--- app/src/main/java/de/ur/mi/jonesses/myapplication4/MainActivity.java
+++ app/src/main/java/de/ur/mi/jonesses/myapplication4/MainActivity.java
@@ -2,6 +2,7 @@ package de.ur.mi.jonesses.myapplication4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Irgendein Log
+        Log.d("TESTLOG", "GitHub Test");
     }
 }
