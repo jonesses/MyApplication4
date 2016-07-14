d54a55861d1369e55b6531df86d160a889e5f046
Jonas <jonesses@gmail.com>
2016-07-14 10:47:29 +0200

    Initial commit

---
+++ app/src/main/java/de/ur/mi/jonesses/myapplication4/MainActivity.java
@@ -0,0 +1,13 @@
+package de.ur.mi.jonesses.myapplication4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
